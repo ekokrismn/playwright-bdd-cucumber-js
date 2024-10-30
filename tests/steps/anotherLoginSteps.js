d81584a8e374013113f5114fff1523d97f8649e2
ekokrismn <ekokrismantoro@gmail.com>
2024-10-30 21:58:19 +0700

    Update anotherLoginSteps.js

--- tests/steps/anotherLoginSteps.js
+++ tests/steps/anotherLoginSteps.js
@@ -1,24 +1,19 @@
 import { createBdd } from 'playwright-bdd';
 import { test } from '../fixtures/fixtures';
-
 const { Given, When, Then } = createBdd(test);
-
 
 Given('I am on the login page', async ({ loginOutline }) => {
     await loginOutline.navigateToLogin();
    
 });
 
-
 When('I enter valid {string} And valid {string}', async ({ loginOutline }, email, password) => {
     await loginOutline.inputCreds();
   });
 
-
 When('I clicks on login button', async ({ loginOutline }) => {
     await loginOutline.clickLoginButton();
 });
-
 
 Then('I should be able to login', async ({ loginOutline }) => {
     await loginOutline.verifyLogin();
